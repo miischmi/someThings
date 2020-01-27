2d244a3dc0bd9b44c46d1a21400d782dd3c3a3bb
Mschmid <60365025+northwolflady@users.noreply.github.com>
2020-01-27 21:35:58 +0100

    Add files via upload
    Generates random and unique numbers

---
+++ NumberGenerator.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberGenerator {
+
+	public static void main(String[] args) {
+		List<Integer> zahlen = new ArrayList<Integer>();
+
+		for (int i = 0; i <= 1000; i++) {
+			int zahl = randomNumber();
+			if (zahlen.contains(zahl)) {
+				i--;
+				continue;
+			} else {
+				zahlen.add(zahl);
+			}
+		}
+		for (int liste : zahlen) {
+			System.out.print(liste + " ");
+		}
+	}
+
+	public static int randomNumber() {
+		int zahl = (int) (Math.random() * (90000 + 1));
+		return zahl;
+	}
+}
